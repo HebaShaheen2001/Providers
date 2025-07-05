f76c950b3a99a16dd4c86eb83376745dcaf3ba6d
Heba Shaheen <106197704+HebaShaheen2001@users.noreply.github.com>
2025-07-05 19:50:55 +0300

    p

--- lib/Screen/Api/Screen/homeApi.dart
+++ lib/Screen/Api/Screen/homeApi.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:providers/Screen/Api/Screen/homeModle.dart';
+import 'package:providers/Screen/Api/Screen/categoriesModle.dart';
 import 'package:providers/Screen/Api/api_setting.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,29 +13,38 @@ class Homeapi extends StatefulWidget {
 }
 
 class _HomeapiState extends State<Homeapi> {
+  //اول شي هان عملت ليست ومررت المودل فيها
   List<categoriesModel> categories = [];
+  // هان متغير بحالة الانتظار
   bool isLoading = true;
+
+// هان فنكشن لجزئية ال api وهان بنعمل الانتظار asyan
   Future<void> fetchCategories() async {
+    // هان بنستخدم ال try catch عشان بحالة الاخطاء
     try {
+      // هان بعرف الرابط وبستخدم مكتبة http لاعرف الرابط
       var url = Uri.parse(ApiSettings.home);
       var response = await http.get(url);
 
+// شرط اذا كان نتيحة الريكوست 200 يعني نجاح بجيب الbody الي هي البيانات
       if (response.statusCode == 200) {
         var jsonBody = json.decode(response.body);
+        // وعرفت ليست جوا حطيت فيها البيانات وحددت بيانات التصميفات فقط
         List data = jsonBody['categories'];
-
+// هنا بعمل تحديث تلقائي وبعرف متغير حطيت فيها البيانات من الريكوست وربطتها بالمودل وملاحظة هان مش متاكدة من فهمي
         setState(() {
           categories = data.map((m) => categoriesModel.fromJson(m)).toList();
+          // هان خليت اللانتظار حالته فولس اذا جاب البيانات يعني خلص بصير فولس
           isLoading = false;
         });
       } else {
-        print("Error: ${response.statusCode}");
+        // هان بحالة صار خطا المفترض احط رسائل خطا
         setState(() {
           isLoading = false;
         });
       }
     } catch (e) {
-      print("Exception: $e");
+      // وهان بحالة في خطا ما بحطه هان بخليها ليفحص الكود
       setState(() {
         isLoading = false;
       });
